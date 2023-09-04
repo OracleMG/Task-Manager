c31ccc792b18d8a6692e4cec02248871fb780191
OracleMG <mihaigraur01@gmail.com>
2023-09-05 00:15:00 +0300

    add props data

--- my-app/src/components/task-card/TaskCard.js
+++ my-app/src/components/task-card/TaskCard.js
@@ -1,16 +1,28 @@
 import './TaskCard.css';
 
-function TaskCard(){
-    return (<div className="card-wrapper">
-        <div className='card-header'>
-            <p>T-1</p>
-            <div className='badge'>
-                <p>Todo</p>
+
+
+function TaskCard(props) {
+   
+    return (
+        <div className="card-wrapper">
+            <div className='card-header'>
+                <p>{props.id}</p>
+                <div className='badge'>
+                    <p>{props.status}</p>
+                </div>
             </div>
-        </div>
-        <div className='card-content'></div>
-        <div className='card-footer'></div>
-        Some text
+            <div className='card-content'>
+                <p>{props.name}</p>
+            </div>
+            <div className='card-footer'>
+                <div>
+                    <p className='card-footer-text'>Due date</p>
+                    <p>{props.dueDate.toLocaleDateString()}</p>
+                </div>
+
+            </div>
+
         </div>);
 }
 
